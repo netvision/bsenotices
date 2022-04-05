abe5f0dbd7757b5c34442fb431afba367a6cc437
Rakesh Jangid <rakesh@jangid.co.in>
2022-04-05 06:19:45 +0530

    first trial

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,28 @@
 <script setup>
-import ButtonRepo from '@/components/ButtonRepo.vue'
+import {ref, onMounted} from 'vue'
+import axios from 'axios'
+import moment from 'moment'
+const baseUrl = ref('https://www.bseindia.com/xml-data/corpfiling/AttachLive/')
+
+const notices = ref()
+onMounted(async() =>{
+  let notice = await axios.get('https://api.bseindia.com/BseIndiaAPI/api/AnnGetData/w?pageno=1&strCat=-1&strPrevDate=&strScrip=&strSearch=P&strToDate=&strType=C')
+  notices.value = notice.data.Table
+  let ofs = await axios.get('https://www1.nseindia.com/live_market/content/live_watch/offer_sale/ofs_details_retail.json')
+  console.log(ofs)
+})
 </script>
 
 <template>
   <div class="bg-gray-50">
-    <div
-      class="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-16 lg:px-8"
-    >
-      <h2
-        class="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 sm:text-4xl sm:leading-10"
-      >
-        Ready to dive in?
-        <br />
-        <span class="text-indigo-600">Vite + Vue 3 + Tailwind CSS</span>
-      </h2>
-      <div class="mt-8 flex lg:mt-0 lg:flex-shrink-0">
-        <div class="inline-flex rounded-md shadow">
-          <router-link
-            to="/about"
-            class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-5 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out hover:bg-indigo-500 focus:outline-none"
-            >Next Page</router-link
-          >
-        </div>
-        <ButtonRepo />
-      </div>
+    <div class="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-16 lg:px-8">
+    <ul v-if="notices">
+      <li v-for="notice in notices" :key="notice.NEWSID" class="py-8">
+        <a target="_blank" :href = "baseUrl+notice.ATTACHMENTNAME"> {{ notice.NEWSSUB}} </a><br />
+        <span>{{ moment(notice.NEWS_DT).format('DD-MM-YYYY @ hh:mm')}}</span>
+      </li>
+    </ul>
     </div>
+    <pre>{{notices}}</pre>
   </div>
 </template>
